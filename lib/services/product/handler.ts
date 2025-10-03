c1bf98eaf04f1baf12cc4b4fb12b54c6f1f61ca5
Jose D. Vasquez <jodavaga@gmail.com>
2025-10-03 10:29:28 -0500

    feat: join stock and products list from dynamoDb. test locally with sam

--- lib/services/product/handler.ts
+++ lib/services/product/handler.ts
@@ -1,11 +1,13 @@
 import { APIGatewayEvent, Context } from "aws-lambda";
-import {} from "@aws-sdk/util-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 import {
   DynamoDBClient,
   GetItemCommand,
   ScanCommand,
 } from "@aws-sdk/client-dynamodb";
 
+const productsTableName = process.env.PRODUCTS_TABLE_NAME;
+const stockTableName = process.env.STOCK_TABLE_NAME;
 export interface ResponseProxyType {
   statusCode: number;
   headers: Record<string, string>;
@@ -15,7 +17,6 @@ export interface ResponseProxyType {
 const dynamoDBClient = new DynamoDBClient({
   region: process.env.AWS_REGION || "us-east-1",
 });
-const productsTable = process.env.PRODUCTS_TABLE_NAME;
 
 export async function getAllProducts(
   event: APIGatewayEvent,
@@ -23,14 +24,14 @@ export async function getAllProducts(
 ): Promise<ResponseProxyType> {
   try {
     const getAllProductsCommand = new ScanCommand({
-      TableName: productsTable,
+      TableName: productsTableName,
+    });
+    const getStockCommand = new ScanCommand({
+      TableName: stockTableName,
     });
 
     const getProductsResult = await dynamoDBClient.send(getAllProductsCommand);
-    console.log(
-      "🚀 ~ getAllProducts ~ getProductsResult:",
-      unmarshall(getProductsResult)
-    );
+    const stockResult = await dynamoDBClient.send(getStockCommand);
 
     if (!getProductsResult) {
       return {
@@ -38,9 +39,33 @@ export async function getAllProducts(
         body: `All Products not found`,
       } as ResponseProxyType;
     }
+    if (!stockResult) {
+      return {
+        statusCode: 404,
+        body: `Stock not found`,
+      } as ResponseProxyType;
+    }
+
+    const productsList = getProductsResult.Items?.map((product) =>
+      unmarshall(product)
+    );
+    const stockList = stockResult.Items?.map((stockItem) =>
+      unmarshall(stockItem)
+    );
+
+    const productsStockJoined = productsList?.map((product) => ({
+      ...product,
+      count: stockList?.find((stockItem) => stockItem.product_id === product.id)
+        ?.count,
+    }));
+
+    console.log(
+      "🚀 ~ getAllProducts ~ productsStockJoined:",
+      productsStockJoined
+    );
 
     const response = {
-      data: getProductsResult,
+      data: productsStockJoined,
       statusCode: 200,
     };
 
@@ -70,7 +95,7 @@ export async function getProduct(
     // const product = products.find((p) => p.id === productId);
 
     const getProductCommand = new GetItemCommand({
-      TableName: productsTable,
+      TableName: productsTableName,
       Key: {
         id: { S: productId || "" },
       },
